e209d2ecc9b760d2f71f6b492ad76662e998d818
Oscar Andersson <oscar.andersson89@gmail.com>
2015-04-10 15:35:26 +0200

    Fixed caching, functionality to index PDF-files
    Caching does not work, at least not in Sitecore 7.5. The only text that was printed from cache is "System.Xml.XmlTextWriter". Caching is fixed using a custom StringWriter and also added code to index files, PDF is set as example. Added code to only index files that are viewable by anonymous users

--- SitemapXmlGenerator.cs
+++ SitemapXmlGenerator.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Linq;
-using Sitecore.Pipelines.HttpRequest;
-using Sitecore.Diagnostics;
 using Sitecore;
-using Sitecore.Links;
-using System.Xml;
-using Sitecore.Data.Items;
+using Sitecore.ContentSearch.Linq.Utilities;
+using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
 using Sitecore.Layouts;
+using Sitecore.Links;
+using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
 using System.Web.Caching;
+using System.Xml;
+using System.Linq.Expressions;
+using Sitecore.ContentSearch.Linq;
+using PerstorpCom.Search.Sublayouts;
+using Sitecore.Resources.Media;
+using System.IO;
+using System.Text;
+
 
 namespace SitecoreFromArg.SimpleSitemapXml
 {
+    public sealed class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding { get { return Encoding.UTF8; } }
+    }
+
     public class SitemapXmlGenerator : HttpRequestProcessor
     {
         public string sitemapUrl { get; set; }
@@ -37,35 +54,75 @@ namespace SitecoreFromArg.SimpleSitemapXml
             {
                 args.Context.Response.Write(sitemapXmlCache.ToString());
                 args.Context.Response.End();
+
                 return;
             }
 
-            // 
+            //
             var options = LinkManager.GetDefaultUrlOptions();
             options.AlwaysIncludeServerUrl = true;
 
             // Creating the XML Header
-            var xml = new XmlTextWriter(args.Context.Response.Output);
+            var sw = new Utf8StringWriter();
+            //var xml = new XmlTextWriter(args.Context.Response.Output);
+
+            var xml = new XmlTextWriter(sw);
             xml.WriteStartDocument();
             xml.WriteStartElement("urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
             // Creating the XML Body
             try
             {
-                var items = Context.Database.SelectItems("fast:" + Context.Site.RootPath + "//*");
+                string anonymousUser = @"extranet\Anonymous";
+                Sitecore.Security.Accounts.User scUser = Sitecore.Security.Accounts.User.FromName(anonymousUser, false);
 
-                foreach (var item in items)
+                using (new Sitecore.Security.Accounts.UserSwitcher(scUser))
                 {
-                    if (IsPage(item))
+                    var items = Context.Database.SelectItems("fast:" + Context.Site.RootPath + "//*");
+
+                    foreach (var item in items)
                     {
-                        if (!item.Paths.IsContentItem) continue;
-                        if (excludedPaths.Split('|').Any(p => item.Paths.ContentPath.Contains(p))) continue;
-                        xml.WriteStartElement("url");
-                        xml.WriteElementString("loc", LinkManager.GetItemUrl(item, options));
-                        xml.WriteElementString("lastmod", item.Statistics.Updated.ToString("yyyy-MM-ddThh:mm:sszzz"));
-                        xml.WriteEndElement();
+                        if (IsPage(item))
+                        {
+                            if (!item.Paths.IsContentItem) continue;
+                            if (excludedPaths.Split('|').Any(p => item.Paths.ContentPath.Contains(p))) continue;
+                            xml.WriteStartElement("url");
+                            xml.WriteElementString("loc", LinkManager.GetItemUrl(item, options));
+                            xml.WriteElementString("lastmod", item.Statistics.Updated.ToString("yyyy-MM-ddThh:mm:sszzz"));
+                            xml.WriteEndElement();
+                        }
+                    }
+
+                    var siteIndex = Sitecore.ContentSearch.ContentSearchManager.GetIndex("sitecore_web_index");
+
+                    using (var siteSearchContext = siteIndex.CreateSearchContext())
+                    {
+                        var filePredicate = PredicateBuilder.True<SearchResultItem>();
+                        filePredicate = filePredicate.And(p => p.TemplateName == "Pdf");
+                        var fileResult = siteSearchContext.GetQueryable<SearchResultItem>().Where(filePredicate).GetResults();
+                        //MediaUrlOptions muo = new MediaUrlOptions();
+                        //muo.AlwaysIncludeServerUrl = true;
+
+                        foreach (var file in fileResult)
+                        {
+                            //Get item to only index files that can be viewed/downloaded by anonymous user
+                            var item = (MediaItem)file.Document.GetItem();
+
+                            if (item != null)
+                            {
+                                if (excludedPaths.Split('|').Any(p => file.Document.Url.Contains(p))) continue;
+                                xml.WriteStartElement("url");
+                                xml.WriteElementString("loc", Globals.ServerUrl + file.Document.Url);
+                                xml.WriteElementString("lastmod", file.Document.Updated.ToString("yyyy-MM-ddThh:mm:sszzz"));
+                                xml.WriteEndElement();
+                            }
+                            //MediaManager.GetMediaUrl((MediaItem)item, murl);
+                            //var titleField = file.Document.GetField("title");
+                        }
                     }
                 }
+                args.Context.Response.Write(sw.ToString());
+
             }
             finally
             {
@@ -74,13 +131,15 @@ namespace SitecoreFromArg.SimpleSitemapXml
                 xml.Flush();
 
                 // Cache XML content
-                args.Context.Cache.Add("sitemapxml", xml.ToString(), null,
+                args.Context.Cache.Add("sitemapxml", sw.ToString(), null,
                               DateTime.Now.AddSeconds(int.Parse(cacheTime)),
                               Cache.NoSlidingExpiration,
                               CacheItemPriority.Normal,
                               null);
 
                 args.Context.Response.Flush();
+                //args.Context.Response.SuppressContent = true;
+                //args.Context.ApplicationInstance.CompleteRequest();
                 args.Context.Response.End();
             }
         }
